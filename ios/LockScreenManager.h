1abe25800beb143b4cfee3b8bb1d1d1d36f83fc9
takuu <taku30@gmail.com>
2017-02-03 09:08:48 -0800

    initial commit

---
+++ ios/LockScreenManager.h
@@ -0,0 +1,13 @@
+//
+//  LockScreenManager.h
+//  react-native-now-play
+//
+//  Created by Taku Uechi on 2/3/17.
+//
+//
+
+#ifndef LockScreenManager_h
+#define LockScreenManager_h
+
+
+#endif /* LockScreenManager_h */
---
+++ ios/LockScreenManager.m
@@ -0,0 +1,9 @@
+//
+//  LockScreenManager.m
+//  react-native-now-play
+//
+//  Created by Taku Uechi on 2/3/17.
+//
+//
+
+#import <Foundation/Foundation.h>
